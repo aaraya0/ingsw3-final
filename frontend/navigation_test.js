40d0eecc9b58211ace3802e4b27f1c8157e359c4
aaraya <agusaraya131@gmail.com>
2024-02-08 09:49:51 -0300

    cambios

--- frontend/navigation_test.js
+++ frontend/navigation_test.js
@@ -9,4 +9,4 @@ Scenario('should display menu products', ({ I }) => {
   I.seeElement('.product-card'); // Verificar que al menos un producto se muestra
   I.seeNumberOfElements('.product-card', 5); // Verificar que hay 4 productos
   
-});
+}).config({ "waitForTimeout": 40000 }); 
